c506df45b8dbeddce8a75da21a01d8ecd2c5b355
yxiong <yxiong@fnz.com>
2022-04-26 23:59:42 +0800

    dead lock

---
+++ ch01/Concurrency.Cookbook/Concurrency.Cookbook/DeadLock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Concurrency.Cookbook
+{
+    public class DeadLock
+    {
+        public DeadLock()
+        {
+  
+        }
+
+        async  Task WaitAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+
+        void Deadlock()
+        {
+            Task task = WaitAsync();
+
+            //Dead lock
+            task.Wait();
+        }
+    }
+}
